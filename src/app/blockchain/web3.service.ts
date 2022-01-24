48fe8dc2a2d0104a60c742a83a3745b4de8df521
Jude Martin <judemartin.mj@gmail.com>
2022-01-25 00:24:57 +0530

    writing to blockchain

---
+++ src/app/blockchain/web3.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import Web3 from 'web3';
+import { Contract } from 'web3-eth-contract';
+declare var window: any;
+const contractAbi = require('./contractABI.json');
+@Injectable({
+  providedIn: 'root',
+})
+export class Web3Service {
+  private web3: Web3 = null as unknown as Web3;
+  private contract: Contract = null as unknown as Contract;
+  private contractAddress = '0x77c935D5Dc493601294705E1c254599ff240eB80';
+  //0x4cA6ea7716E31692BB0c1Fce3b4F32B5e54bFC68
+  constructor() {
+    if (window.web3) {
+      this.web3 = new Web3(window.ethereum);
+      this.contract = new this.web3.eth.Contract(
+        contractAbi,
+        this.contractAddress
+      );
+      window.ethereum.enable().catch((error: any) => {
+        console.log(error);
+      });
+    } else {
+      console.warn('Metamask not found. Install or enable Metamask.');
+    }
+  }
+
+  async getAccount(): Promise<string> {
+    return this.web3.eth.getAccounts().then((accounts) => accounts[0] || '');
+  }
+
+  async executeTx(fnName: string, ...args: any[]): Promise<void> {
+    const acc = await this.getAccount();
+    this.contract.methods[fnName](...args).send({ from: acc });
+  }
+  //exexuteTx("vote", pollId, vote)
+  //exexuteTx("createPoll", question , thumg, opt)
+}
