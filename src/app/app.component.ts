eb55759e54b5e6aba388f733e3e50763fdefb100
Jude Martin <judemartin.mj@gmail.com>
2022-01-24 16:17:46 +0530

    poll component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'blockchain-poll';
+  showForm = false;
+  polls = [
+    {
+      question: 'Do you like dogs of cats?',
+      pollImage:
+        'https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg',
+      votes: [0, 5, 7],
+      voted: true,
+    },
+    {
+      question: 'Best month for summer holidays>',
+      pollImage:
+        'https://images.pexels.com/photos/1118448/pexels-photo-1118448.jpeg',
+      votes: [1, 6, 4],
+      voted: false,
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PollCreateComponent } from './poll-create/poll-create.component';
+import { PollComponent } from './poll/poll.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PollCreateComponent, PollComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
