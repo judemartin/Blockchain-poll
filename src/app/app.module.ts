3a2c6653ba0500863e7ed82abcbe38747949cb9a
Jude Martin <judemartin.mj@gmail.com>
2022-01-24 16:30:10 +0530

    Poll vote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
 import { PollComponent } from './poll/poll.component';
+import { PollVoteComponent } from './poll-vote/poll-vote.component';
 
 @NgModule({
-  declarations: [AppComponent, PollCreateComponent, PollComponent],
+  declarations: [AppComponent, PollCreateComponent, PollComponent, PollVoteComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
